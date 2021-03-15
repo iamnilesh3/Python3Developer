50b63100b25f70473566b3b5ba1555c243b8e6b8
Nilesh Singh <41994696+iamnilesh3@users.noreply.github.com>
2021-03-15 16:18:30 +0530

    Create strings.py

---
+++ Basics 1/strings.py
@@ -0,0 +1,13 @@
+print('Hello Nilesh')
+username = 'Nilesh'
+password = 'Secret'
+long_string = '''
+Hi nilesh
+this is a
+long string '''
+print(long_string)
+
+first_name = "Nilesh"
+last_name = "Singh"
+full_name = first_name + " " + last_name
+print(full_name)
