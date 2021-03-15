7b0c5fc4227886eb1bfc5779dc5e808780e8c4de
Nilesh Singh <41994696+iamnilesh3@users.noreply.github.com>
2021-03-15 16:59:52 +0530

    Create string_slicing.py

---
+++ Basics 1/string_slicing.py
@@ -0,0 +1,27 @@
+name = "0123456789"
+
+# [start:stop:stepover]
+
+print(name[1])
+print(name[-0])
+print(name[-1])
+print(name[2:])
+print(name[:5])
+print(name[1:])
+print(name[:])
+print(name[::1])
+print(name[::-1])
+print(name[::2])
+print(name[::-2])
+
+python = 'I am PYHTON'
+
+print(python[1:4])
+print(python[1:])
+print(python[:])
+print(python[1:100])
+print(python[-1])
+print(python[-4])
+print(python[:-3])
+print(python[-3:])
+print(python[::-1])
